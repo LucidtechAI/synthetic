038a4d28074dc59dabf6a9043ad182400046bfc9
augustak <august@lucidtech.ai>
2022-05-12 11:01:48 +0200

    Updated comment

--- synthetic/pdf/utils.py
+++ synthetic/pdf/utils.py
@@ -37,9 +37,10 @@ class Font:
             for k, v in unicode_map.cid2unichr.items():
                 reverse_lookup[v] = k
 
-        # Whitespace will sometimes be encoded as soft-hyphen, which does not look correct in the output PDF
-        if reverse_lookup.get(' ') == 173:
-            reverse_lookup[' '] = 32
+        # Whitespace will sometimes be encoded as soft-hyphen (173), which does not look correct in the output PDF
+        whitespace = ' '
+        if reverse_lookup.get(whitespace) == 173:
+            reverse_lookup[whitespace] = ord(whitespace)
 
         return reverse_lookup
 
