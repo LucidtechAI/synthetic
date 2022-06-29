50b56f41a70d0f6573ec42e94d79443c4319b894
stinebuu <stine.vennemo@gmail.com>
2022-06-29 17:06:59 +0200

    better names, add command line values, use Counter, clean up

--- examples/exclude-words/create_wordlist.py
+++ examples/exclude-words/create_wordlist.py
@@ -1,21 +1,22 @@
 import argparse
 import glob
 import json
-import pathlib
+from collections import Counter
+from pathlib import Path
 from pdfminer.high_level import extract_text
 
 
 def main(args):
     file_path = args.file_path
-    frequency = int(args.minimal_frequency)
-    minimal_length = int(args.minimal_length)
+    minimum_frequency = args.minimum_frequency
+    minimum_length = args.minimum_length
+    allow_numeric = args.allow_numeric
     words_file = args.words_file
 
-    if not file_path[-1] == '/':
-        file_path += '/'
-
-    word_dict = {}
-    for file in glob.glob(file_path + '*.pdf'):
+    word_dict = Counter()
+    for file in file_path.iterdir():
+        if file.suffix != '.pdf':
+            continue
         text = extract_text(file)
         sentence_list = text.split('\n')
         sentence_list = [sentence for sentence in sentence_list if sentence]
@@ -23,14 +24,15 @@ def main(args):
         for sentence in sentence_list:
             for word in sentence.split(' '):
                 word = word.lower()
-                if len(word) > 1 and word[-1] == ':':
-                    word = word[:-1]
-                if not word.isnumeric() and len(word) >= minimal_length:
-                    word_dict[word] = word_dict.get(word, 0) + 1
+                word.replace(':', '')
+                if not allow_numeric and word.isnumeric():
+                    continue
+                if len(word) >= minimum_length:
+                    word_dict[word] += 1
 
     word_list = []
     for word, word_frequency in word_dict.items():
-        if word_frequency >= frequency:
+        if word_frequency >= minimum_frequency:
             word_list.append(word)
 
     words_file.write_text(json.dumps(word_list, indent=2))
@@ -43,12 +45,18 @@ if __name__ == '__main__':
         'when documents are synthesized.',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument('file_path', help='path to folder containing the files from which to create wordlist')
-    parser.add_argument('minimal_frequency', help='minimal frequency for which to add word to word.json')
-    parser.add_argument('--minimal_length', help='minimal length of words added to the word list', default=3)
-    parser.add_argument('--words_file',
-                        help='json file containing the wordlist',
-                        default=pathlib.Path(__file__).parent / 'words.json')
+    parser.add_argument(
+        'file_path', type=Path,
+        help='path to folder containing the files from which to create wordlist',
+    )
+    parser.add_argument('minimum_frequency', type=int, help='minimum frequency for which to add word to word.json')
+    parser.add_argument('--minimum_length', type=int, help='minimum length of words added to the word list', default=3)
+    parser.add_argument('--allow_numeric', action='store_true', help='let numeric strings be added to word list')
+    parser.add_argument(
+        '--words_file',
+        help='json file containing the wordlist',
+        default=Path(__file__).parent / 'words.json',
+    )
     arguments = parser.parse_args()
 
     main(arguments)
